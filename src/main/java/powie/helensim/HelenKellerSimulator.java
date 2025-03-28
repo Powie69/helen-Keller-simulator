3b099652198b6d44d5f2c18b50fd848b581567ad
Powie <PowieIsCool@gmail.com>
2025-03-28 12:20:52 +0800

    update stuff

--- src/main/java/powie/helensim/HelenKellerSimulator.java
+++ src/main/java/powie/helensim/HelenKellerSimulator.java
@@ -1,19 +1,16 @@
 package powie.helensim;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.sound.SoundCategory;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
 
 public class HelenKellerSimulator implements ModInitializer {
 	public static final String MOD_ID = "helen-keller-simulator";
@@ -24,12 +21,11 @@ public class HelenKellerSimulator implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
-		HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> layeredDrawer.attachLayerBefore(IdentifiedLayer.CHAT, KELLER_LAYER, HelenKellerSimulator::render));
+		LOGGER.info("Loading Helen Keller Simulator");
+		HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> layeredDrawer.attachLayerBefore(IdentifiedLayer.MISC_OVERLAYS , KELLER_LAYER, HelenKellerSimulator::render));
 	}
 
-
 	private static void render(DrawContext context, RenderTickCounter tickCounter) {
-		context.fill(0, 0, client.getWindow().getScaledWidth(), client.getWindow().getScaledHeight(), 0, 0xFF000000);
+		context.fill(0, 0, client.getWindow().getScaledWidth(), client.getWindow().getScaledHeight(), 1, 0xFF000000);
 	}
 }
